58219608ad012da42af0af9fd5597a3feac59048
yongan.zhang <yongcan.zhang@ibm.com>
2021-02-25 15:22:52 -0400

    add spinner before get list of crops

--- liquid-prep-app/src/app/components/select-crop/select-crop.module.ts
+++ liquid-prep-app/src/app/components/select-crop/select-crop.module.ts
@@ -6,17 +6,19 @@ import { SelectCropComponent } from './select-crop.component';
 import { MaterialModule } from '../../material/material.module';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [SelectCropComponent],
-  imports: [
-    CommonModule,
-    FlexLayoutModule,
-    SelectCropRoutingModule,
-    MaterialModule,
-    FormsModule,
-    Ng2SearchPipeModule
-  ]
+    imports: [
+        CommonModule,
+        FlexLayoutModule,
+        SelectCropRoutingModule,
+        MaterialModule,
+        FormsModule,
+        Ng2SearchPipeModule,
+        MatProgressSpinnerModule
+    ]
 })
 export class SelectCropModule { }

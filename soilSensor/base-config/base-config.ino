8db7c2f7dc7530c36483e93388997cd6bb916fcc
Gaurav Ramakrishna <gaurav.ramakrishna@ibm.com>
2021-03-11 15:23:36 -0500

    modified micro-controller code to send soil moisture percentage
    Signed-off-by: Gaurav Ramakrishna <gaurav.ramakrishna@ibm.com>

--- soilSensor/base-config/base-config.ino
+++ soilSensor/base-config/base-config.ino
@@ -2,8 +2,15 @@ void setup() {
   Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
 }
 void loop() {
+  //For UNO R3 micro-controller the max and min moisture reading are;
+  int max = 220;
+  int min = 530;
   int val;
   val = analogRead(0); //connect sensor to Analog 0
-  Serial.println(val); //print the value to serial port
+
+  int valueMinDiff = abs(val - min);
+  int maxMinDiff = abs(max - min);
+  float moistPercentage = ((float)valueMinDiff / maxMinDiff) * 100;
+  Serial.println(moistPercentage); //print the value to serial port
   delay(1000); //wait one second
 }

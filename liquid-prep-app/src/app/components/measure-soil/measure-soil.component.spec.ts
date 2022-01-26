8177a9f7df580e7ec136e6b85c9b6859b0515be8
Jeff Lu <ljeff@us.ibm.com>
2022-01-26 16:06:07 -0500

    incremental update ng to v11

--- liquid-prep-app/src/app/components/measure-soil/measure-soil.component.spec.ts
+++ liquid-prep-app/src/app/components/measure-soil/measure-soil.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MeasureSoilComponent } from './measure-soil.component';
 
@@ -6,7 +6,7 @@ describe('MeasureSoilComponent', () => {
   let component: MeasureSoilComponent;
   let fixture: ComponentFixture<MeasureSoilComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MeasureSoilComponent ]
     })
